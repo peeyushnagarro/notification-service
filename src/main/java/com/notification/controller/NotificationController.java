1086ae08101f3ac8e00d092a49d67590fbe6c2d2
ppaneri <ppaneri@tillster.com>
2022-01-13 11:37:13 +0530

    files added

--- src/main/java/com/notification/controller/NotificationController.java
+++ src/main/java/com/notification/controller/NotificationController.java
@@ -1,7 +1,7 @@
 package com.notification.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +10,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @RestController
-@RequestMapping(path="/api/notification/")
+@RequestMapping(path="/api/notifications")
 public class NotificationController {
 
 	private static final Logger logger = LogManager.getLogger(NotificationController.class);
 	
-	@GetMapping("publishNotification/{customerId}")
+	@PostMapping("/{customerId}")
 	public void publishNotification(@PathVariable int customerId) {
 		logger.info("Notification published for customer id : - " + customerId);
 	}
